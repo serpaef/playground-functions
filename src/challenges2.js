0cbeb5111d01540e56ae5420851c903678bc8acb
Fernando Serpa <serpaef@outlook.com>
2021-06-29 18:19:56 -0300

    Requisito #10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 10
-function techList() {
+// precisei buscar uma função que ordenasse o array antes de gerar os objetos
+// encontrei esse tutorial:
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+function techList(arr, name) {
   // seu código aqui
+  orderedArr = arr.sort();
+  let objList = [];
+  if(orderedArr.length !== 0){
+    for (t of orderedArr){
+      objList.push({
+        tech: t,
+        name: name,
+      })
+    }
+    return objList;
+  }
+  return 'Vazio!'
 }
 
 // Desafio 11

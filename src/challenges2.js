e2151f67dd772f8abcf5636978aa2c8f824690c1
Fernando Serpa <serpaef@outlook.com>
2021-06-29 19:01:34 -0300

    Requisito #11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,30 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arr) {
   // seu código aqui
+  let cont = 0;
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let n in arr) {
+    if (arr[n] < 0 || arr[n] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  for (let n1 = 0; n1 < arr.length; n1 += 1) {
+    for (let n2 = 1; n2 < arr.length; n2 += 1) {
+      if (arr[n1] === arr[n2]){
+        cont += 1;
+      }
+    }
+    if (cont > 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    cont = 0;
+  }
+  let phone = '('+ arr[0] + arr [1] + ') ' + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + '-' + arr[7] + arr[8] + arr[9] + arr[10];
+  return phone;
 }
 
 // Desafio 12

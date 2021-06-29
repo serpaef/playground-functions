438b06ec8a5fb90bc8d0b23a31e179feb10ff40e
Fernando Serpa <serpaef@outlook.com>
2021-06-29 16:27:21 -0300

    Requisito #6

--- src/challenges.js
+++ src/challenges.js
@@ -39,8 +39,25 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(arr) {
   // seu código aqui
+  let ord = arr;
+  let count = 0
+  for (let n2 = 1; n2 < ord.length; n2 +=1) {
+    for (let n1 = 0; n1 < ord.length; n1 += 1) {
+      if(ord[n2] > ord[n1]) {
+        let aux = ord[n1];
+        ord[n1] = ord[n2];
+        ord[n2] = aux;
+      }
+    }
+  }
+  for (let index = 0; index < arr.length; index +=1){
+    if (ord[0] === arr[index]){
+      count += 1;
+    }
+  }
+  return count;
 }
 
 // Desafio 7
